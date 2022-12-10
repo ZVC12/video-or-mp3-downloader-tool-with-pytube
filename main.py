d49a3e190a2e4e60fdccff12b7fc775acb25bbf8
ZVC12 <83571661+ZVC12@users.noreply.github.com>
2022-12-10 10:07:43 +0000

    UPDATE

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from pytube import YouTube
 import pyfiglet
 import os
-print(pyfiglet.figlet_format("ZVC",font = "banner3-D" ))
-print("mp4(1)\nmp3(2)\ncikis(3)")
-try:
+import sys
+def yazi():
+    print(pyfiglet.figlet_format("ZVC",font = "banner3-D" ))
+yazi()
+print("(1)Turkish\n(2)English\n ")
+dil = input("please choose your language : ")
+if dil == "1":
+    yazi()
+    print("mp4(1)\nmp3(2)\ncikis(3)")
     download = int(input("indirmek istediğiniz dosya türünü seçiniz : "))
-
     if download == 1:
         def mp4():
             link = str(input("linki giriniz : "))
@@ -13,7 +18,7 @@ try:
             stream =yt.streams.get_highest_resolution()
             stream.download()
             print("video indirildi")
-        mp4()
+            mp4()
     elif download == 2:
         def mp3():
             link = str(input("linki giriniz : "))
@@ -23,12 +28,34 @@ try:
             ad,uza = os.path.splitext(indirilmis)
             yeni = ad + '.mp3'
             os.rename(indirilmis, yeni)
+            print("görüsmek üzere ! ")
+            mp3()
+    elif download == 3:
+        sys.exit()
+elif dil == "2":
+    yazi()
+    print("mp4(1)\nmp3(2)\nexit(3)")
+    download = int(input("please select the file type you want to download :  "))
+    if download == 1:
+        def mp4():
+            link = str(input("please enter link : "))
+            yt = YouTube(link)
+            stream =yt.streams.get_highest_resolution()
+            stream.download()
+            print("video downloaded")
+        mp4()
+    elif download == 2:
+        def mp3():
+            link = str(input("please enter link : "))
+            yt = YouTube(link)
+            stream =yt.streams.filter(only_audio=True).first()
+            indirilmis = stream.download()
+            ad,uza = os.path.splitext(indirilmis)
+            yeni = ad + '.mp3'
+            os.rename(indirilmis, yeni)
+            print("mp3 downloaded")
         mp3()
     elif download == 3:
-        quit()
-
-
-except(ValueError()):
-    print("lütfen menüde belirtilen rakamlardan birini seçiniz !")
-
-    
+        print("goodbye ! ")
+        sys.exit()
+       
